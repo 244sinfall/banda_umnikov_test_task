915c39266a452695df4de5a7628d7594e078edb5
Dmitry Filin <dimafilin6@icloud.com>
2023-07-20 18:46:48 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/banda_umnikov_test_task/" : "/",
+  publicPath: '/'
 })
